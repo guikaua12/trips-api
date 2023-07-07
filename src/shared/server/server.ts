c07632e74ea5e86aac8417bbe0bb3cff68efaf8b
Approximations <guilhermeksilva556@gmail.com>
2023-07-07 17:52:52 -0300

    fixed error handling

--- src/shared/server/server.ts
+++ src/shared/server/server.ts
@@ -1,21 +1,20 @@
 import 'express-async-errors';
-import express, {NextFunction, Request, Response} from 'express';
-// import {router} from '../routes';
-// import {AppError} from '../errors/AppError';
-
+import express, { NextFunction, Request, Response } from 'express';
+import { router } from '@/shared/routes';
+import { AppError } from '@/shared/errors/AppError';
 
 const server = express();
 server.use(express.json());
-// server.use(router);
+server.use(router);
 
 // error handling
-server.use((err: any , req: Request, res: Response, next: NextFunction) => {
-    // if(err instanceof AppError) {
-    //     return res.status(err.status).json({message: err.message});
-    // }
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+        return res.status(err.status).json({ error: true, message: err.message });
+    }
 
-    console.log(err);
-    res.status(500).json({message: 'Internal Server Error'});
+    console.error(err);
+    res.status(500).json({ error: true, message: 'Internal Server Error' });
 });
 
-export {server};
+export { server };
