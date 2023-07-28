6fa37c2250593ad8a6c6c489f48011698d0c7daf
Approximations <guilhermeksilva556@gmail.com>
2023-07-28 19:33:42 -0300

    Create user now returns a session id

--- src/modules/users/controllers/CreateUserController.ts
+++ src/modules/users/controllers/CreateUserController.ts
@@ -1,19 +1,23 @@
 import { Request, Response } from 'express';
 import { CreateUserUseCase } from '@/modules/users/useCases/CreateUserUseCase';
-import { generateJwt } from '@/shared/utils/jwt';
+import { randomHash } from '@/shared/utils/randomUtils';
+import { CreateSessionUseCase } from '@/modules/sessions/useCases/CreateSessionUseCase';
 export class CreateUserController {
-    constructor(private useCase: CreateUserUseCase) {}
+    constructor(
+        private useCase: CreateUserUseCase,
+        private createSessionUseCase: CreateSessionUseCase
+    ) {}
 
     async handle(req: Request, res: Response): Promise<void> {
         const { email, password } = req.body;
 
         const user = await this.useCase.execute({ email, password });
-        const jwt = generateJwt({ id: user.id, email: user.email });
+        const session = await this.createSessionUseCase.execute({ session: randomHash(), user_id: user.id });
 
         res.status(200).json({
             id: user.id,
             email: user.email,
-            token: jwt,
+            session: session.session,
         });
     }
 }
--- src/modules/users/factories/CreateUserFactory.ts
+++ src/modules/users/factories/CreateUserFactory.ts
@@ -2,9 +2,15 @@ import { UserRepository } from '@/modules/users/repositories/UserRepository';
 import { pool } from '@/shared/database';
 import { CreateUserUseCase } from '@/modules/users/useCases/CreateUserUseCase';
 import { CreateUserController } from '@/modules/users/controllers/CreateUserController';
+import { SessionRepository } from '@/modules/sessions/repositories/SessionRepository';
+import { CreateSessionUseCase } from '@/modules/sessions/useCases/CreateSessionUseCase';
 
 export function createUserFactory() {
     const userRepository = new UserRepository(pool);
     const createUserUseCase = new CreateUserUseCase(userRepository);
-    return new CreateUserController(createUserUseCase);
+
+    const sessionRepository = new SessionRepository(pool);
+    const createSessionUseCase = new CreateSessionUseCase(sessionRepository);
+
+    return new CreateUserController(createUserUseCase, createSessionUseCase);
 }
