a01d3102f7de304d0191dd7d3904869a0fa3d8d3
Approximations <guilhermeksilva556@gmail.com>
2023-07-28 13:26:12 -0300

    zod error catch

--- src/modules/users/useCases/SearchUserUseCase.ts
+++ src/modules/users/useCases/SearchUserUseCase.ts
@@ -1,12 +1,20 @@
 import { IUserRepository } from '@/modules/users/repositories/IUserRepository';
 import { User } from '@/modules/users/models/User';
 import { AppError } from '@/shared/errors/AppError';
-import { SearchUserDTO } from '@/modules/users/dtos/SearchUserDTO';
+import { SearchUserDTO, SearchUserDTOSchema } from '@/modules/users/dtos/SearchUserDTO';
+import { ZodError } from 'zod';
+import { zodToString } from '@/shared/utils';
 
 export class SearchUserUseCase {
     constructor(private userRepository: IUserRepository) {}
 
     async execute({ id, email }: SearchUserDTO): Promise<User> {
+        try {
+            SearchUserDTOSchema.parse({ id, email });
+        } catch (error) {
+            if (error instanceof ZodError) throw new AppError(400, zodToString(error));
+        }
+
         const user = await this.userRepository.search({ id, email });
 
         if (!user) throw new AppError(404, 'User not found');
