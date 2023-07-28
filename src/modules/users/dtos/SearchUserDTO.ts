945afef33a3c9145a7b18dbc17a6f20b9bfda674
Approximations <guilhermeksilva556@gmail.com>
2023-07-28 13:26:23 -0300

    format

--- src/modules/users/dtos/SearchUserDTO.ts
+++ src/modules/users/dtos/SearchUserDTO.ts
@@ -1,17 +1,8 @@
 import { z } from 'zod';
 
 export const SearchUserDTOSchema = z.object({
-    id: z
-        .string({
-            invalid_type_error: 'Invalid id type',
-        })
-        .optional(),
-    email: z
-        .string({
-            invalid_type_error: 'Invalid email type',
-        })
-        .email('Invalid email')
-        .optional(),
+    id: z.string({ invalid_type_error: 'Invalid id type' }).optional(),
+    email: z.string({ invalid_type_error: 'Invalid email type' }).email('Invalid email').optional(),
 });
 
 export type SearchUserDTO = z.infer<typeof SearchUserDTOSchema>;
