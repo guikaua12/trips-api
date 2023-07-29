4d164a903dc472b0ec3f965f6e753cfeeb3e0e9a
Approximations <guilhermeksilva556@gmail.com>
2023-07-29 15:48:39 -0300

    made create route public

--- src/modules/users/routes/index.ts
+++ src/modules/users/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import { loginUserFactory } from '@/modules/users/factories/LoginUserFactory';
+import { createUserFactory } from '@/modules/users/factories/CreateUserFactory';
 
 const userRouter = Router();
 
 const loginUserController = loginUserFactory();
+const createUserController = createUserFactory();
 
 userRouter.post('/login', async (req, res) => {
     await loginUserController.handle(req, res);
+});
+
+userRouter.post('/create', async (req, res) => {
+    await createUserController.handle(req, res);
 });
 
 export { userRouter };
