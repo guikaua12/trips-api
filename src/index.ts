915b0be8db7d6488abaeb9c86e2d1a15aa4f84b8
Approximations <datkidbosst@gmail.com>
2023-09-04 04:50:47 +0000

    added .env.local to dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { server } from '@/shared/server/server';
 import { connect } from '@/shared/database';
 require('dotenv').config();
+require('dotenv').config({ path: '.env.test' });
 
 async function main() {
     await connect();
