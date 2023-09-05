558412966bc19c62d4038480dd010180138393e0
Approximations <datkidbosst@gmail.com>
2023-09-05 14:11:10 -0300

    added unit tests for TripRepository

--- src/modules/trips/repositories/TripRepository.spec.ts
+++ src/modules/trips/repositories/TripRepository.spec.ts
@@ -1,34 +1,114 @@
 import { connect } from '@/shared/database';
 
-require('dotenv').config();
-import { Pool } from 'pg';
-import process from 'process';
-import { test, expect, afterAll } from '@jest/globals';
-import { TripRepository } from './TripRepository';
+import { test, expect, beforeAll } from '@jest/globals';
+import { tripRepository } from '@/shared/repositories';
+import { data } from '@/shared/seed';
 
-export const pool = new Pool({
-    connectionString: process.env.CONNECTION_URL,
+beforeAll(async () => {
+    await connect();
+    await tripRepository!.deleteAll();
+    await tripRepository!.createMany(data);
 });
 
-export async function connect() {
-    return pool.connect((err) => {
-        if (err) {
-            console.error(err);
-            process.exit();
-            return;
-        }
-
-        console.log('Database connected');
-    });
-}
-
 test('getAllByIds works', async () => {
-    await connect();
-    const trips = await new TripRepository(pool).getAllByIds(['1', '43666154-ba45-4bba-97c9-ba6a4e289c7e']);
+    const trips = await tripRepository!.getAllByIds(['1', 'b5b08318-0ffd-4587-9f05-a2965cc7a9ac']);
 
     expect(trips.length).toBe(1);
 });
 
-afterAll(() => {
-    process.exit(1);
+test('search by location works', async () => {
+    let trips = await tripRepository!.search({ location: 'Hotel' });
+    expect(trips.length).toBe(2);
+
+    trips = await tripRepository!.search({ location: 'hotel aurora' });
+    expect(trips.length).toBe(1);
+
+    trips = await tripRepository!.search({ location: 'italia' });
+    expect(trips.length).toBe(2);
+});
+
+test('search by pricePerDay works', async () => {
+    let trips = await tripRepository!.search({ pricePerDay: 700 });
+    expect(trips.length).toBe(9);
+
+    trips = await tripRepository!.search({ pricePerDay: 300 });
+    expect(trips.length).toBe(4);
+
+    trips = await tripRepository!.search({ pricePerDay: 250 });
+    expect(trips.length).toBe(3);
+});
+
+test('search by recommended works', async () => {
+    let trips = await tripRepository!.search({ recommended: true });
+    expect(trips.length).toBe(4);
+
+    trips = await tripRepository!.search({ recommended: false });
+    expect(trips.length).toBe(5);
+});
+
+test('get by id works', async () => {
+    const trip = await tripRepository!.getById('b5b08318-0ffd-4587-9f05-a2965cc7a9ac');
+
+    expect(trip).toBeDefined();
+    expect(trip?.id).toEqual('b5b08318-0ffd-4587-9f05-a2965cc7a9ac');
+    expect(trip?.name).toEqual('Hotel Aurora');
+    expect(trip?.location).toEqual('Amalfi, Itália');
+    expect(trip?.countryCode).toEqual('IT');
+    expect(trip?.imagesUrl).toEqual([
+        'https://images.unsplash.com/photo-1538683270504-3d09ad7ae739?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        'https://images.unsplash.com/photo-1528215747454-3d0e0902fff2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80',
+        'https://images.unsplash.com/photo-1534612899740-55c821a90129?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    ]);
+    expect(trip?.pricePerDay).toEqual(250);
+    expect(trip?.highlights).toEqual([
+        'Café da manhã incluso',
+        'Piscina',
+        'Wifi grátis',
+        'Estacionamento grátis',
+        'Vista paradisíaca',
+        'Luxuoso',
+    ]);
+    expect(trip?.recommended).toEqual(true);
+    expect(trip?.maxGuests).toEqual(5);
+});
+
+test('create works', async () => {
+    const dto = {
+        id: 'c25828ad-a079-4572-8b09-c4158ac95a47',
+        name: 'Some hotel',
+        description: 'Some description',
+        startDate: new Date('2023-07-03'),
+        endDate: new Date('2023-07-30'),
+        location: 'Some location',
+        countryCode: 'BR',
+        coverImage:
+            'https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-4.0.3&ixid=M3wxMA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        imagesUrl: [
+            'https://images.unsplash.com/photo-1538683270504-3d09ad7ae739?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        ],
+        pricePerDay: 1337,
+        highlights: [
+            'Café da manhã incluso',
+            'Piscina',
+            'Wifi grátis',
+            'Estacionamento grátis',
+            'Vista paradisíaca',
+            'Luxuoso',
+        ],
+        recommended: false,
+        maxGuests: 10,
+    };
+
+    const trip = await tripRepository!.create(dto);
+
+    expect(trip).toBeDefined();
+    expect(trip?.id).toEqual(dto.id);
+    expect(trip?.name).toEqual(dto.name);
+    expect(trip?.location).toEqual(dto.location);
+    expect(trip?.countryCode).toEqual(dto.countryCode);
+    expect(trip?.imagesUrl).toEqual(dto.imagesUrl);
+    expect(trip?.pricePerDay).toEqual(dto.pricePerDay);
+    expect(trip?.highlights).toEqual(dto.highlights);
+    expect(trip?.recommended).toEqual(dto.recommended);
+    expect(trip?.maxGuests).toEqual(dto.maxGuests);
 });
--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -6,23 +6,24 @@ import pg, { Pool } from 'pg';
 import { init, tripRepository, tripReservationRepository, userRepository } from '@/shared/repositories';
 pg.types.setTypeParser(pg.types.builtins.NUMERIC, (value: string) => parseFloat(value));
 
-export const pool = new Pool({
+export let pool = new Pool({
     connectionString: process.env.CONNECTION_URL,
 });
 
 export async function connect() {
-    return pool.connect((err) => {
-        if (err) {
-            console.error(err);
-            process.exit();
-            return;
-        }
+    try {
+        await pool.connect();
 
         init(pool);
-        createTables();
-        setupExtensions();
+        await createTables();
+        await setupExtensions();
         console.log('Database connected');
-    });
+    } catch (err) {
+        console.error(err);
+    }
+}
+export async function disconnect() {
+    await pool.end();
 }
 
 export async function createTables() {
