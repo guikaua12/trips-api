48c05cf99e2e6abe02610a261b6d7b2c1a80b441
Approximations <datkidbosst@gmail.com>
2023-09-06 18:31:07 -0300

    remove `isIsoDateString` from dateUtils.ts, now using `isValid` from date-fns

--- src/shared/utils/dateUtils.ts
+++ src/shared/utils/dateUtils.ts
@@ -1,17 +1,11 @@
-import { parseISO } from 'date-fns';
-
-const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
-
-export function isIsoDateString(value: any): boolean {
-    return value && typeof value === 'string' && isoDateFormat.test(value);
-}
+import { parseISO, isValid } from 'date-fns';
 
 export function handleDates(body: any) {
-    if (body === null || body === undefined || typeof body !== 'object') return body;
+    if (!body || typeof body !== 'object') return body;
 
     for (const key of Object.keys(body)) {
         const value = body[key];
-        if (isIsoDateString(value)) body[key] = parseISO(value);
+        if (isValid(value)) body[key] = parseISO(value);
         else if (typeof value === 'object') handleDates(value);
     }
 }
