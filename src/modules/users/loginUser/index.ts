5a7dc6f9a7034f1ac04215c9c7d9193fd5e82ae1
Approximations <datkidbosst@gmail.com>
2023-09-08 00:10:30 -0300

    added `BcryptPasswordVerify` as dependency to `LoginUserUseCase`

--- src/modules/users/loginUser/index.ts
+++ src/modules/users/loginUser/index.ts
@@ -2,9 +2,11 @@ import { UserRepository } from '@/modules/users/repositories/UserRepository';
 import { pool } from '@/shared/database';
 import { LoginUserUseCase } from '@/modules/users/loginUser/useCase';
 import { LoginUserController } from '@/modules/users/loginUser/controller';
+import { BcryptPasswordVerify } from '../passwordVerify/BcryptPasswordVerify';
 
 const userRepository = new UserRepository(pool);
-const loginUserUseCase = new LoginUserUseCase(userRepository);
+const passwordVerify = new BcryptPasswordVerify();
+const loginUserUseCase = new LoginUserUseCase(userRepository, passwordVerify);
 
 const loginUserController = new LoginUserController(loginUserUseCase);
 
